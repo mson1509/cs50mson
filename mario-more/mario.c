ea52add4cc37e22405aca3a405722d8e577f8041
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-08-21 09:04:16 +0000

    Mon, Aug 21, 2023, 4:04 PM +07:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -10,6 +10,20 @@ do
     height = get_int("Height of the pyramids is: ");
 }
 while (height < 1 || height > 8);
- // Print the pyramids
 
+ // Print the pyramids
+int row;
+for (row = 1; row <= height; row++)
+{
+    for (i = 0; i < height - row; i++)
+    {
+        printf(" ");
+    }
+    for (j = 0; j < row; j++)
+    {
+        printf(" ");
+    }
+    printf("  ");
+    for (h = 0; )
+}
 }
