4fc7e4cf57c732a88fc6ad364b12ea460bf24917
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-08-21 09:08:49 +0000

    Mon, Aug 21, 2023, 4:08 PM +07:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,31 +3,31 @@
 
 int main(void)
 {
- // Ask for the pyramids' height
-int height;
-do
-{
-    height = get_int("Height of the pyramids is: ");
-}
-while (height < 1 || height > 8);
+    // Ask for the pyramids' height
+    int height;
+    do
+    {
+        height = get_int("Height of the pyramids is: ");
+    }
+    while (height < 1 || height > 8);
 
- // Print the pyramids
-int row;
-for (row = 1; row <= height; row++)
-{
-    for (int i = 0; i < height - row; i++)
+    // Print the pyramids
+    int row;
+    for (row = 1; row <= height; row++)
     {
-        printf(" ");
+        for (int i = 0; i < height - row; i++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < row; j++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int h = 0; h < row; h++)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
-    for (int j = 0; j < row; j++)
-    {
-        printf("#");
-    }
-    printf("  ");
-    for (int h = 0; h < row; h++)
-    {
-        printf("#");
-    }
-    printf("\n");
-}
 }
