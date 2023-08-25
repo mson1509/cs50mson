b5250236b3edc18c5c5d4a5c5d8ff8ec304a3de5
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-08-25 09:05:26 +0000

    Fri, Aug 25, 2023, 4:05 PM +07:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -6,7 +6,14 @@
 int main(int argc, string argv[])
 {
     // check whether the key is right
-    
+    if (argc != 2)
+    {
+        printf("Usage: ./substitution key\n");
+    }
+    else if (argc argv[2] != 26)
+    {
+        printf("Key must contain 26 characters.\n");
+    }
     // identify each characters of text
     // substitute it by the given key
 }
