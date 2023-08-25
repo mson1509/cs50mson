0e902d81057e54cd3f31d2c849568ad1be7e2011
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-08-25 17:19:13 +0000

    Sat, Aug 26, 2023, 12:19 AM +07:00

---
+++ substitution/draft.c
@@ -0,0 +1,31 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+string key = "YTNSHKVEFXRBAUQZCLWDMIPGJO";
+bool only_once_letter(key)
+{
+    int exclusive_letter_count = 0;
+    key = convert_lower(key);
+    bool verify;
+    for (char letter = 'a'; letter <= 'z'; letter++)
+    {
+        for (int i = 0; i < strlen(key); i++)
+        {
+            if (key[i] == letter)
+            {
+                exclusive_letter_count++;
+                break;
+            }
+        }
+    }
+    if (exclusive_letter_count == 26)
+    {
+        verify = true;
+    }
+    else
+    {
+        verify = false;
+    }
+    return verify;
+}
