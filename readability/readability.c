0e2dfff3577df3cf712a200dece2361221f9364c
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-08-25 07:20:27 +0000

    Fri, Aug 25, 2023, 2:20 PM +07:00

--- readability/readability.c
+++ readability/readability.c
@@ -10,7 +10,8 @@ int main(void)
     // prompt the user for the text
     string text = get_string("Text: ");
     // calculate reading level
-    int reading_level = 0.0588 * letters - 0.296 * sentences - 15.8
+    int reading_level = 0.0588 * (letters / word) * 100 - 0.296 * (sentences / word) * 100 - 15.8;
+    printf(" Grade %i\n", reading_level);
 }
     // count number of letters
 int num_letters(string text)
