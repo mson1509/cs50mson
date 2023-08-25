6fdda858994672b771bb77b1ae025de278b2d328
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-08-25 07:53:11 +0000

    Fri, Aug 25, 2023, 2:53 PM +07:00

--- readability/readability.c
+++ readability/readability.c
@@ -10,9 +10,9 @@ int main(void)
     // prompt the user for the text
     string text = get_string("Text: ");
     // calculate reading level
-    int letters = num_letters( text);
-    int words = num_words(string text);
-    int sentences = num_sentences(string text);
+    int letters = num_letters(text);
+    int words = num_words(text);
+    int sentences = num_sentences(text);
     int reading_level = 0.0588 * (letters / words) * 100 - 0.296 * (sentences / words) * 100 - 15.8;
     printf(" Grade %i\n", reading_level);
 }
@@ -20,7 +20,7 @@ int main(void)
 int num_letters(string text)
 {
     int length = 0;
-    for (int n = 0; text[n] != /0; n++)
+    for (int n = 0; text[n] != '\0'; n++)
     {
         if (text[n] > 64 && text[n] < 91) || (text[n] > 96 && text[n] < 123)
         {
@@ -33,7 +33,7 @@ int num_letters(string text)
 int num_words(string text)
 {
     int spaces = 1;
-    for (int n = 0; text[n] != /0; n++)
+    for (int n = 0; text[n] != '\0'; n++)
     {
         if (text [n] == ' ')
         {
@@ -46,7 +46,7 @@ int num_words(string text)
 int num_sentences(string text)
 {
     int marks = 0;
-    for (int n = 0; text[n] != /0; n++)
+    for (int n = 0; text[n] != '\0'; n++)
     {
         if (text[n] == '.' || text[n] == '!' || text[n] == '?')
         {
