1c66a45a9b5a3e1cefa6a1f7e09de67a4998ae72
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-08-25 07:44:29 +0000

    Fri, Aug 25, 2023, 2:44 PM +07:00

--- readability/readability.c
+++ readability/readability.c
@@ -10,31 +10,40 @@ int main(void)
     // prompt the user for the text
     string text = get_string("Text: ");
     // calculate reading level
-    int reading_level = 0.0588 * (letters / word) * 100 - 0.296 * (sentences / word) * 100 - 15.8;
+    int letters = num_letters(string text);
+    int words = num_words(string text);
+    int sentences = num_sentences(string text);
+    int reading_level = 0.0588 * (letters / words) * 100 - 0.296 * (sentences / words) * 100 - 15.8;
     printf(" Grade %i\n", reading_level);
 }
     // count number of letters
 int num_letters(string text)
 {
     int length = 0;
-    while ((text[n] > 64 && text[n] < 91) || (text[n] > 96 && text[n] < 123))
+    for (int n = 0; text[n] != /0; n++)
     {
-        length++;
+        if (text[n] > 64 && text[n] < 91) || (text[n] > 96 && text[n] < 123)
+        {
+            length++;
+        }
     }
     return length;
 }
     // count number of words
 int num_words(string text)
 {
-    int spaces;
-    for (int n = 0; text [n] = ' '; n++)
+    int spaces = 1;
+    for (int n = 0; text[n] != /0; n++)
+    while(text [n] == ' ')
     {
-
+        if (text[n] == ' ')
+        spaces++;
     }
-
+    return spaces;
 }
     // count number of sentences
 int num_sentences(string text)
 {
+    int 
 
 }
