0f391c3ef49a7831185a4c49ccf9c32b4dfb428a
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-08-25 07:12:47 +0000

    Fri, Aug 25, 2023, 2:12 PM +07:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int num_letters()
+
 int main(void)
 {
-    
+    // prompt the user for the text
+    string text = get_string("Text: ");
+    // calculate reading level
+
 }
+    // count number of letters
+    // count number of words
+    // count number of sentences
