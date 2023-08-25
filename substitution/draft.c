fff93a6b0f24a31ec0ec5a6e8dd05367e0a3a9d1
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-08-25 18:17:53 +0000

    Sat, Aug 26, 2023, 1:17 AM +07:00

--- substitution/draft.c
+++ substitution/draft.c
@@ -7,27 +7,10 @@ bool only_once_letter(string key);
 
 int main(void)
 {
-    string key = "YTNSHKVEFXRBAUQZCLWDMIPGJO";
-    int exclusive_letter_count = 0;
-    bool verify;
-    for (char letter = 'A'; letter <= 'Z'; letter++)
+    string text = "YTNSHKVEFXRBAUQZCLWDMIPGJO";
+    for (int i = 0; text[i] < strlen(text); i++)
     {
-        for (int i = 0; i < strlen(key); i++)
-        {
-            if (key[i] == letter)
-            {
-                exclusive_letter_count++;
-                break;
-            }
-        }
+        text[i] = tolower(text[i]);
     }
-    if (exclusive_letter_count == 26)
-    {
-        verify = true;
-    }
-    else
-    {
-        verify = false;
-    }
-    printf("%i %i\n", exclusive_letter_count, verify);
+    printf("%s\n", text);
 }
