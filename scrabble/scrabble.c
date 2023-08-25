2200a0387ab547b4b06daebf734c2bfbdc221f65
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-08-25 04:17:58 +0000

    Fri, Aug 25, 2023, 11:17 AM +07:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -24,5 +24,10 @@ int main(void)
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
-    
+    int n = strlen(word);
+    for (i = 0; i < n; i ++)
+    {
+        if word [i] 
+    }
+
 }
