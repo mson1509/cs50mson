ef106b15f63ddebc533bb743927268ed1b98366e
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-08-25 09:30:26 +0000

    Fri, Aug 25, 2023, 4:30 PM +07:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,9 +3,12 @@
 #include <stdio.h>
 #include <string.h>
 
+bool non_alphabetic(string argv[2]);
+bool only_once_letter(string argv[2]);
+
 int main(int argc, string argv[])
 {
-    // check whether the key is right
+    // check whether the key is valid
     if (argc != 2)
     {
         printf("Usage: ./substitution key\n");
@@ -14,20 +17,30 @@ int main(int argc, string argv[])
     {
         printf("Key must contain 26 characters.\n");
     }
-    else if (isupper(argv[2]) == false || islower(argv[2]) == false)
+    else if (non_alphabetic(argv[2]) == false)
     {
         printf("Key must not contain non-alphabetic characters.\n");
     }
-    else if ()
+    else if (only_once_letter(argv[2]) == false)
+    {
+        printf("Key must have each letter exactly once.\n");
+    }
     else
     {
         string plain_text = get_string("Plaintext: ");
     }
+    // check the non-alphabetic characters in key
+bool non_alphabetic(string argv[2])
+{
+
+}
+    // check whether each letter appear only once in key
+bool only_once_letter(string argv[2])
+{
+
+}
     // identify upper characters and convert by key to upper cyphertext
-
     // identify lower characters and convert by key to lower cyphertext
-
     // identify non-alphabetic characters and remain same to cyphertext
-
     // print cyphertext
 }
