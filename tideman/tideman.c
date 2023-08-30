85282dbb3dfd43af1b6631312116b29145091d42
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-08-30 16:00:22 +0000

    Wed, Aug 30, 2023, 11:00 PM +07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -106,6 +106,7 @@ bool vote(int rank, string name, int ranks[])
                 for (int j = 0; j < candidate_count; j++)
                 {
                     ranks[j] = i;
+                    break;
                 }
             }
     }
