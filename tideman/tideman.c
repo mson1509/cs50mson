b61505fd1d803f7a48a076dd44cc430bb296d58d
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-08-31 09:54:18 +0000

    Thu, Aug 31, 2023, 4:54 PM +07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -182,7 +182,14 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    
+    for (int i = 0; i < pair_count; i++)
+    {
+        if ()
+        {
+            continue;
+        }
+        locked[pairs[i].winner][pairs[i].loser] == true;
+    }
     return;
 }
 
