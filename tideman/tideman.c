a6fe8937c3207e31963ee0d48dece7c981894afc
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-08-31 10:45:42 +0000

    Thu, Aug 31, 2023, 5:45 PM +07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -135,7 +135,7 @@ void add_pairs(void)
             {
                 continue;
             }
-            else if (preferences[i][j] > preferences[j][i])
+            if (preferences[i][j] > preferences[j][i])
             {
                 pairs[pair_count].winner = i;
                 pairs[pair_count].loser = j;
