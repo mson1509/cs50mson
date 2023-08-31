93ce49e5111b3a99fa201e4cb56362546cca8b5f
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-08-31 10:21:35 +0000

    Thu, Aug 31, 2023, 5:21 PM +07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -185,6 +185,7 @@ void lock_pairs(void)
     for (int x = 0; x < pair_count; x++)
     {
         locked[pairs[x].winner][pairs[x].loser] == true;
+        //check if cycle was created
         for (int i = 0; i < candidate_count; i++)
         {
             for (int j = 0; j < candidate_count; j++)
@@ -193,11 +194,13 @@ void lock_pairs(void)
                 {
                     break;
                 }
-                else if (j == candidate_count)
+                //if there is still a source, keep lock pairs
+                else if (j == candidate_count - 1)
                 {
                     i = MAX + 1;
                 }
             }
+            // if there is no source left, unlock the last pair
             if (i == candidate_count - 1)
             {
                 locked[pairs[x].winner][pairs[x].loser] == false;
