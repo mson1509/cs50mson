d5abf78ec3dc090b53bf9b998185b3fed4002469
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-08-31 15:56:38 +0000

    Thu, Aug 31, 2023, 10:56 PM +07:00

--- tideman/test.c
+++ tideman/test.c
@@ -1,3 +1,38 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Max number of candidates
+#define MAX 9
+
+// preferences[i][j] is number of voters who prefer i over j
+int preferences[MAX][MAX];
+
+// locked[i][j] means i is locked in over j
+bool locked[MAX][MAX];
+
+// Each pair has a winner, loser
+typedef struct
+{
+    int winner;
+    int loser;
+}
+pair;
+
+// Array of candidates
+string candidates[MAX];
+pair pairs[MAX * (MAX - 1) / 2];
+
+int pair_count;
+int candidate_count;
+
+void lock_pair(void);
+
+int main(void)
+{
+
+}
+
 void lock_pairs(void)
 {
     for (int x = 0; x < pair_count; x++)
