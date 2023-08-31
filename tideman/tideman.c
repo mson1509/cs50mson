4c27c75c6d65e05c657063bed6db2d6116a38092
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-08-31 10:24:20 +0000

    Thu, Aug 31, 2023, 5:24 PM +07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -215,7 +215,13 @@ void print_winner(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if (locked[j][i])
+            {
+                printf
+            }
+        }
     }
     return;
 }
