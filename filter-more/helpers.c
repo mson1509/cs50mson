220b1b2b1d2b1abc65a1d6dbfaa00d55fc365e7b
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-06 09:29:45 +0000

    Wed, Sep 6, 2023, 4:29 PM +07:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -1,5 +1,7 @@
 #include "helpers.h"
 
+void swap(RGBTRIPLE a, RGBTRIPLE b);
+
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -19,20 +21,20 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
         if (width % 2 == 0)
         {
-            for (int j; j < (width / 2); j++)
+            for (int j = 0; j < (width / 2); j++)
             {
-                swap(image[i][j], image[i][width - 1 - j])
+                swap(image[i][j], image[i][width - 1 - j]);
             }
         }
         else
         {
-            for (int j; j < (width - 1) / 2); j++)
+            for (int j = 0; j < ((width - 1) / 2); j++)
             {
-                swap(image[i][j], image[i][width - 1 - j])
+                swap(image[i][j], image[i][width - 1 - j]);
             }
         }
     }
@@ -49,4 +51,11 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
     return;
+}
+
+void swap(RGBTRIPLE a, RGBTRIPLE b)
+{
+    RGBTRIPLE temp = a;
+    a = b;
+    b = temp;
 }
