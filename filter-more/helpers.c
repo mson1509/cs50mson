de7df09c1e0cc39f3eef237afc17431bd063bd68
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-06 09:10:20 +0000

    Wed, Sep 6, 2023, 4:10 PM +07:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -7,7 +7,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j].rgbtBlue 
+            int gray_pixel = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3;
+            image[i][j].rgbtBlue = gray_pixel;
+            image[i]
         }
     }
     return;
