d0a206ac44927a1e977bdff811e2f24f3e6325f5
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-06 04:44:40 +0000

    Wed, Sep 6, 2023, 11:44 AM +07:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -1,7 +1,5 @@
 #include "helpers.h"
 
-char check_b_w(int i, int j, RGBTRIPLE image[i][j]);
-
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
@@ -9,32 +7,16 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            if (check_b_w(i, j, image) == 'w')
+            if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
             {
                 continue;
             }
-            else if (check_b_w(i, j, image) == 'b')
+            else if (image[i][j].rgbtRed == 0xff && image[i][j].rgbtGreen == 0xff && image[i][j].rgbtBlue == 0xff)
             {
                 image[i][j].rgbtRed = 0x15;
                 image[i][j].rgbtGreen = 0x5b;
                 image[i][j].rgbtBlue = 0xfb;
             }
         }
-    }
-}
-
-char check_b_w(int i, int j, RGBTRIPLE image[i][j])
-{
-    if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
-    {
-        return 'w';
-    }
-    else if (image[i][j].rgbtRed == 0xff && image[i][j].rgbtGreen == 0xff && image[i][j].rgbtBlue == 0xff)
-    {
-        return 'b';
-    }
-    else
-    {
-        return 1;
     }
 }
