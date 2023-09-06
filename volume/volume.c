56c1b64298ebb45e40cf96ba06befd2cbc77ce5f
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-06 07:15:49 +0000

    Wed, Sep 6, 2023, 2:15 PM +07:00

--- volume/volume.c
+++ volume/volume.c
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
     while (buffer != EOF)
     {
         fread(&buffer, sizeof(int16_t), 1, input);
-        buffer *=2;
+        buffer *= factor;
         fwrite(&buffer, sizeof(int16_t), 1, output);
     }
 
