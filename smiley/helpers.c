b493b7b463e6892c6fc9fc57ec7a66ff8617a861
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-06 04:25:33 +0000

    Wed, Sep 6, 2023, 11:25 AM +07:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -1,6 +1,36 @@
 #include "helpers.h"
 
+int check_b_w(RGBTRIPLE image[i][j]);
+
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (check_b_w(image[i][j]) == 'w')
+            {
+                continue;
+            }
+            else if (check_b_w(iimage[i][j]) == 'b')
+            {
+                image[i][j].rgbtRed == 0x15;
+                image[i][j].rgbtGreen == 0x5b;
+                image[i][j].rgbtBlue == 0xfb;
+            }
+        }
+    }
+}
+
+char check_b_w(RGBTRIPLE image[i][j])
+{
+    if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
+    {
+        return 'w';
+    }
+    else if (image[i][j].rgbtRed == 0xff && image[i][j].rgbtGreen == 0xff && image[i][j].rgbtBlue == 0xff)
+    {
+        return 'b';
+    }
 }
