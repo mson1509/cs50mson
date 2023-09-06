e588e7b5ff7790d688be4fa6f495a136d7783d67
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-06 07:55:07 +0000

    Wed, Sep 6, 2023, 2:55 PM +07:00

--- volume/volume.c
+++ volume/volume.c
@@ -40,9 +40,8 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
-    while (!feof(input))
+    while (fread(&buffer, sizeof(int16_t), 1, input) == 1)
     {
-        fread(&buffer, sizeof(int16_t), 1, input);
         buffer *= factor;
         fwrite(&buffer, sizeof(int16_t), 1, output);
     }
