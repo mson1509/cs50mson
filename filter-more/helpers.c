0f2ab4464c4aaef483d11dba1130d70c83d9a6fb
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-06 08:59:17 +0000

    Wed, Sep 6, 2023, 3:59 PM +07:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -3,6 +3,10 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j = )
+    }
     return;
 }
 
