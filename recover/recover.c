0131d506c6ff1810b44903ad42cb7d3f768797d9
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-08 07:18:33 +0000

    Fri, Sep 8, 2023, 2:18 PM +07:00

--- recover/recover.c
+++ recover/recover.c
@@ -17,9 +17,7 @@ int main(int argc, char *argv[])
         return 1;
     }
     int* buffer = malloc(sizeof_block);
-    while (buffer != )
-    {
-        fread(buffer, sizeof_block, 1, ptr);
+    fread(buffer, sizeof_block, 1, ptr);
     }
 
 }
