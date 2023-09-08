4a31f7c40b8cadbfa3ab68fb6ada48177adabde8
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-08 07:45:15 +0000

    Fri, Sep 8, 2023, 2:45 PM +07:00

--- recover/recover.c
+++ recover/recover.c
@@ -11,17 +11,17 @@ int main(int argc, char *argv[])
         printf("Usage: ./recover <image>\n");
         return 1;
     }
-    FILE* ptr = fopen("argv[1]", "r");
+    FILE* ptr = fopen(argv[1], "r");
     if (ptr == NULL)
     {
         printf("File cannot be opened");
         return 1;
     }
-    un buffer[sizeof_block]
+    unsigned char buffer[sizeof_block]
     for (int i = 0; i < num_images; i++)
     {
         fread(buffer, sizeof_block, 1, ptr);
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && buffer[3] == 'e')
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && buffer[3] == )
     }
 
 }
