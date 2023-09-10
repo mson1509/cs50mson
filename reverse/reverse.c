c2462e11112a4474d3cf124198a47d07550c7ef3
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-10 12:41:22 +0000

    Sun, Sep 10, 2023, 7:41 PM +07:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -56,6 +56,7 @@ int main(int argc, char *argv[])
 
     // Use get_block_size to calculate size of block
     // TODO #7
+    block_size = get_block_size(header_buffer);
     
 
     // Write reversed audio to file
