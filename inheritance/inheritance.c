8dddca3e452cff032640ac2ff5eb64f9d16924a0
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-12 09:21:59 +0000

    Tue, Sep 12, 2023, 4:21 PM +07:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -53,7 +53,7 @@ person *create_family(int generations)
         p->parents[0] = parent0;
         p->parents[1] = parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        for (char tmp = random_alleles(); tmp != parent0->alleles[0] || tmp != parent0->alleles[0]; tmp = random_alleles())
+        while (char tmp = random_alleles(); tmp != parent0->alleles[0] || tmp != parent0->alleles[0])
         {
             p->alleles[0] = tmp;
         }
