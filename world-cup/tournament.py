ccb47f1d2c6b570d605657a48f7b5b350e22b8d3
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-15 07:06:30 +0000

    Fri, Sep 15, 2023, 2:06 PM +07:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,6 +16,10 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
+    file = open("sys.argv[1]", "r")
+    reader = csv.DictReader(file)
+    for row in reader:
+        
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
