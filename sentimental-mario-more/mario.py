e2d27987344c627163de6d7132d349c8e21e735b
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-15 09:02:45 +0000

    Fri, Sep 15, 2023, 4:02 PM +07:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,3 +1,8 @@
 # TODO
 from cs50 import get_int
-height = get_
+height = get_int("Height: ")
+while height < 0:
+    height = get_int("Height ")
+for row in range(height):
+    for spaces in range(height - row):
+        print(" ")
