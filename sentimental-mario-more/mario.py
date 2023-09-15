b6de1d7d836acfaa4da085dca9b25da76c58c8fe
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-15 09:10:49 +0000

    Fri, Sep 15, 2023, 4:10 PM +07:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,14 +1,15 @@
 # TODO
 from cs50 import get_int
+
 height = get_int("Height: ")
 while height < 0 or height > 8:
     height = get_int("Height ")
 for row in range(1, height + 1, 1):
     for spaces in range(height - row):
-        print(" ", end = "")
+        print(" ", end="")
     for first in range(row):
-        print("#", end = "")
-    print ("  ",end = "")
+        print("#", end="")
+    print("  ", end="")
     for second in range(row):
-        print("#",end ="")
+        print("#", end="")
     print()
