15b84667530d5317cf911f0d8c4adb0474fc3f8e
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-15 09:30:17 +0000

    Fri, Sep 15, 2023, 4:30 PM +07:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,3 +2,7 @@
 from cs50 import get_int, get_string
 def main():
     text = get_string("Text: ")
+    letters = num_letters(text)
+    words = num_words(text)
+    sentences = num_sentences(text)
+    
