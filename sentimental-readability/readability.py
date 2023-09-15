3a07089795226802fdec078f014ec8de478d1dee
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-15 09:36:39 +0000

    Fri, Sep 15, 2023, 4:36 PM +07:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,4 +5,8 @@ def main():
     letters = num_letters(text)
     words = num_words(text)
     sentences = num_sentences(text)
+    level = 0.0588 * letters / words * 100 - 0.296 * sentences / words * 100 - 15.8
+    print("Grade: " + level)
+
+def num_letters(text)
     
