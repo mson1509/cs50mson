192541a267bbfffc87fc695250c1157524b225e9
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-15 07:40:40 +0000

    Fri, Sep 15, 2023, 2:40 PM +07:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,10 +16,10 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
-    file = open(sys.argv[1]", "r")
+    file = open(sys.argv[1], "r")
     reader = csv.DictReader(file)
     for row{} in reader:
-        teams.append(row{"team": int("rating")})
+        teams.append(row["team": int("rating")])
     file.close()
 
     counts = {}
