2eda63057b6066ed56b59d405a1785eecba36ea0
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-15 09:43:10 +0000

    Fri, Sep 15, 2023, 4:43 PM +07:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -6,7 +6,35 @@ def main():
     words = num_words(text)
     sentences = num_sentences(text)
     level = 0.0588 * letters / words * 100 - 0.296 * sentences / words * 100 - 15.8
-    print("Grade: " + level)
+    if level > 16:
+        print("Grade: 16+")
+    elif level < 1:
+        print("Before Grade 1")
+    else:
+        print ("Grade: " + level)
+
 
 def num_letters(text)
-    
+    letters = 0
+    for c in text:
+        if c.isalpha():
+            letters += 1
+    return letters
+
+
+def num_words(text)
+    words = 1
+    for c in text:
+        if c == " ":
+            words += 1
+    return words
+
+
+def num_sentences(text)
+    sentences = 0
+    for c in text:
+        if c == "." or c == "?" or c == "!":
+            sentences += 1
+    return sentences
+
+
