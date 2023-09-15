764a67712aaa308c2a5e4184936cd94340da677b
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-15 07:56:49 +0000

    Fri, Sep 15, 2023, 2:56 PM +07:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -58,7 +58,11 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-
+    if len(simulate_round(teams)) == 1:
+        return teams
+    else
+        winners = stimulate_round(teams)
+        simulate_round(winners)
 
 if __name__ == "__main__":
     main()
