b84d61b58bf98729baae69985cbd5b4a555e1ff4
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-19 09:01:35 +0000

    Tue, Sep 19, 2023, 4:01 PM +07:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,23 +1,18 @@
 SELECT DISTINCT
-    (people.name)
+  (people.name)
 FROM
-    people
-JOIN stars
-    ON people.id = stars.person_id
-JOIN movies
-    ON movies.id = stars.movie_id
+  people
+  JOIN stars ON people.id = stars.person_id
+  JOIN movies ON movies.id = stars.movie_id
 WHERE
-    people.name != 'Kevin Bacon'
-    AND movies.title IN
-    (
-        SELECT
-            movies.title
-        FROM
-            movies
-        JOIN stars
-            ON movies.id = stars.movie_id
-        JOIN people
-            ON people.id = stars.person_id
-        AND people.name = 'Kevin Bacon'
-        AND people.birth = 1958
-    );
+  people.name != 'Kevin Bacon'
+  AND movies.title IN (
+    SELECT
+      movies.title
+    FROM
+      movies
+      JOIN stars ON movies.id = stars.movie_id
+      JOIN people ON people.id = stars.person_id
+      AND people.name = 'Kevin Bacon'
+      AND people.birth = 1958
+  );
