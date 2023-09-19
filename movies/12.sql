51795759b8801e8f6b5c3ffced2042e62cf23cc3
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-19 08:33:40 +0000

    Tue, Sep 19, 2023, 3:33 PM +07:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,22 +1,29 @@
 SELECT
-  title
+  *
 FROM
-  movies,
-  stars,
-  people
-WHERE
-  movies.id = stars.movie_id
-  AND people.id = stars.person_id
-  AND name = 'Bradley Cooper';
-SELECT
-  title
-FROM
-  movies,
-  stars,
-  people
-WHERE
-  movies.id = stars.movie_id
-  AND people.id = stars.person_id
-  AND name = 'Jennifer Lawrence';
+  (
+    SELECT
+      title
+    FROM
+      movies,
+      stars,
+      people
+    WHERE
+      movies.id = stars.movie_id
+      AND people.id = stars.person_id
+      AND name = 'Bradley Cooper'
+  ) AS brad_movies
 INNER JOIN
-    
+  (
+    SELECT
+      title
+    FROM
+      movies,
+      stars,
+      people
+    WHERE
+      movies.id = stars.movie_id
+      AND people.id = stars.person_id
+      AND name = 'Jennifer Lawrence'
+  ) AS jen_movies
+ON brad_movies.title = jen_movies.title;
