eb3bd3cba1e3cc313b2834835ae6fae781363284
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-19 08:28:52 +0000

    Tue, Sep 19, 2023, 3:28 PM +07:00

--- movies/12.sql
+++ movies/12.sql
@@ -8,7 +8,6 @@ WHERE
   movies.id = stars.movie_id
   AND people.id = stars.person_id
   AND name = 'Bradley Cooper';
-
 SELECT
   title
 FROM
@@ -19,3 +18,5 @@ WHERE
   movies.id = stars.movie_id
   AND people.id = stars.person_id
   AND name = 'Jennifer Lawrence';
+INNER JOIN
+    
