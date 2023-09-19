acacb79aa03b44ecb5b4fa30d79fa1a010547a2a
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-19 09:34:00 +0000

    Tue, Sep 19, 2023, 4:34 PM +07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,12 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-
+-- Step 1: Find the report of the CS50 Duck theft
+SELECT
+  description
+FROM
+  crime_scene_reports
+WHERE
+  YEAR = 2021
+  AND MONTH = 7
+  AND DAY = 28
+  AND street = 'Humphrey Street';
+-- 2:
