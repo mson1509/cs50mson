830db87584c3efa75ef1c1b1e786f3558da21b8f
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-20 07:29:53 +0000

    Wed, Sep 20, 2023, 2:29 PM +07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,59 +9,63 @@ WHERE
   AND MONTH = 7
   AND DAY = 28
   AND street = 'Humphrey Street';
+
 -- Step 2: Find the name and transcript of the 3 witnesses
 SELECT
-    name, transcript
+  name,
+  transcript
 FROM
-    interviews
+  interviews
 WHERE
-    year = 2021
-    AND month = 7
-    AND day = 28
-    AND transcript LIKE '%thief%';
+  YEAR = 2021
+  AND MONTH = 7
+  AND DAY = 28
+  AND transcript LIKE '%thief%';
+
 -- Step 3: Find the license plate of the thief at the bakery according to witness 1
-CREATE TEMPORARY TABLE
-  thief_license_plates
-AS
+CREATE TEMPORARY TABLE thief_license_plates AS
 SELECT
   license_plate
 FROM
   bakery_security_logs
 WHERE
   activity = 'exit'
-  AND year = 2021
-  AND month = 7
-  AND day = 28
-  AND hour = 10
-  AND minute > 5
-  AND minute < 25;
+  AND YEAR = 2021
+  AND MONTH = 7
+  AND DAY = 28
+  AND HOUR = 10
+  AND MINUTE > 5
+  AND MINUTE < 25;
+
 -- Step 4: Find the account number of the thief according to witness 2
-CREATE TEMPORARY TABLE
-  thief_account_numbers
-AS
+CREATE TEMPORARY TABLE thief_account_numbers AS
 SELECT
   account_number
 FROM
   atm_transactions
 WHERE
-  year = 2021
-  AND month = 7
-  AND day = 28
+  YEAR = 2021
+  AND MONTH = 7
+  AND DAY = 28
   AND atm_location = 'Leggett Street'
   AND transaction_type = 'withdraw';
+
 -- Step 5: Find the airpot the thief gonna go to tommorow according to witness 3
 SELECT
-  airports.abbreviation, airports.full_name, airports.city, flights.id, flights.hour, flights.minute
+  airports.abbreviation,
+  airports.full_name,
+  airports.city,
+  flights.id,
+  flights.hour,
+  flights.minute
 FROM
   flights
-JOIN
-  airports ON airports.id = flights.destination_airport_id
+  JOIN airports ON airports.id = flights.destination_airport_id
 WHERE
-  year = 2021
-  AND month = 7
-  AND day = 29
-  AND flights.origin_airport_id =
-  (
+  YEAR = 2021
+  AND MONTH = 7
+  AND DAY = 29
+  AND flights.origin_airport_id = (
     SELECT
       id
     FROM
@@ -69,6 +73,10 @@ WHERE
     WHERE
       city = 'Fiftyville'
   )
-ORDER BY hour, minute
-LIMIT 1;
+ORDER BY
+  HOUR,
+  MINUTE
+LIMIT
+  1;
+
 -- Step 6: Find the passport number of the thief from his flight_id
