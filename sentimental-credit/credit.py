884521ec1828908a05475602f659aa214a9a34b9
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-26 10:16:01 +0000

    Tue, Sep 26, 2023, 5:16 PM +07:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,3 +2,8 @@
 from cs50 import get_string
 # take the user input
 number = get_string("Number: ")
+
+def every_other_digit(number, from_last_digit):
+    current_digit = true
+    if from_last_digit:
+        number %= 10;
