2f733cfbfcb6516c815b84c8461cde0fc244fa17
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-26 15:40:28 +0000

    Tue, Sep 26, 2023, 10:40 PM +07:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,15 +4,18 @@ from cs50 import get_string
 number = get_string("Number: ")
 
 def every_other_digit(number, from_last_digit):
+    digits = []
     current_digit = from_last_digit
     for i in range(len(str(number))):
         if current_digit:
             digit = number % (10 ** (i + 1))
+            digits.append(digit)
             current_digit = not current_digit
             i += 1
-            if digit * 2 > 9:
-                
         else:
             current_digit = not current_digit
             i += 1
+    return digits
+
+def sum_of_digits
 
