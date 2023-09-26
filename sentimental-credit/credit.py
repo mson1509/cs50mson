763f0d2ae68fbb1d790b68f4dd68ee32f18b302e
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-26 10:18:08 +0000

    Tue, Sep 26, 2023, 5:18 PM +07:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,6 +4,8 @@ from cs50 import get_string
 number = get_string("Number: ")
 
 def every_other_digit(number, from_last_digit):
-    current_digit = true
-    if from_last_digit:
-        number %= 10;
+    current_digit = True
+    if from_last_digit
+        if current_digit:
+            number %= 10;
+            current_digit = not current_digit
