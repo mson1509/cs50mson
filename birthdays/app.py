8e3a7fa2017a21b65df84902f67af622390b81cd
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-27 09:54:59 +0000

    Wed, Sep 27, 2023, 4:54 PM +07:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -33,8 +33,8 @@ def index():
             db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", name, month, day)
             return redirect("/")
         else:
-            message = "Invalid"
-            return redirect("/", message=message)
+            flash("Invalid")
+            return redirect("/")
 
     else:
         # TODO: Display the entries in the database on index.html
