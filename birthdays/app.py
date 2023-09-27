e114afb753cd37379764acb440ec010803168dff
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-27 10:00:53 +0000

    Wed, Sep 27, 2023, 5:00 PM +07:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -5,6 +5,7 @@ from flask import Flask, flash, jsonify, redirect, render_template, request, ses
 
 # Configure application
 app = Flask(__name__)
+app.secret_key = 'secretkey?!?!'
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
