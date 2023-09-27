cf4b254abb8223132c18cd2fb156449d2f4b39c0
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-27 09:17:34 +0000

    Wed, Sep 27, 2023, 4:17 PM +07:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -26,7 +26,7 @@ def after_request(response):
 def index():
     if request.method == "POST":
         # TODO: Add the user's entry into the database
-        db.execute("INSERT )
+        db.execute("INSERT INTO birthdays (name, ) )
         return redirect("/")
 
     else:
