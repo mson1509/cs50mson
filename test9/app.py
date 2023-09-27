b84e8e16bff8e7315489e2318d86cca2565f09d2
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-27 04:27:35 +0000

    Wed, Sep 27, 2023, 11:27 AM +07:00

--- test9/app.py
+++ test9/app.py
@@ -2,12 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-SPORTS = [
-    "Basketball",
-    "Soccer",
-    "Ultimate Frisbee"
-]
-
 
 @app.route("/")
 def index():
