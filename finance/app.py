95dc206f52eca0978c2383f11e650de795750f3e
mson1509 <142561847+mson1509@users.noreply.github.com>
2023-09-28 17:00:00 +0000

    Fri, Sep 29, 2023, 12:00 AM +07:00

--- finance/app.py
+++ finance/app.py
@@ -109,8 +109,10 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    
-    return apology("TODO")
+    # TODOOOO
+    if request.methods == "POST":
+    else:
+        return render_template("register.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
